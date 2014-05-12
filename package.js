e6b9a74054567fc8ba17b5ea1f42cc103beffdd2
andrej fritz <a.fritz@betzold.de>
2014-05-12 11:59:55 +0200

    initial

--- package.js
+++ package.js
@@ -1,10 +1,11 @@
 Package.describe({
-  summary: 'Collection helpers'
+  summary: 'PSD.js + PNG.js packed for Meteor.'
 });
-Npm.depends( function(){
-
+Npm.depends({
+  'psd':'0.0.4',
+  'png':'3.0.3'
 });
 Package.on_use(function(api) {
-  api.use('underscore');
-  api.add_files('collection-helpers.js', ['client', 'server']);
+  api.add_files('psd_png.js', ['server']);
+  api.export(['Parser','Png'], server);
 });
